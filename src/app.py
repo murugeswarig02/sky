976578c2f1fd8fbd346688f5ce5e8ff25f9e0ad0
murugeswari <murugeswarig2303@gmail.com>
2026-04-21 12:13:03 +0530

    Delete src directory

--- src/app.py
+++
@@ -1,6 +0,0 @@
-# Create src folder and add a file
-mkdir -p src
-echo "console.log('Hello World');" > src/app.js
-git add src/app.js
-git commit -m "Added app.js to src folder"
-git push origin main
